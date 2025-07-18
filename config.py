ce83fdba64abd366aad6711b418780824475ecaf
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2025-07-18 09:53:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    PORT: int
+    PORT: int = 8080
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     GROQ_API_KEY: str
